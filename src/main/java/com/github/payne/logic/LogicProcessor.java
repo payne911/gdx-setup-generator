d28ba0de33998c4f8d49c0286a9d6e92fc875596
payne911 <payne911@users.noreply.github.com>
2020-09-15 10:19:11 -0400

    Fixed a build problem

--- src/main/java/com/github/payne/logic/LogicProcessor.java
+++ src/main/java/com/github/payne/logic/LogicProcessor.java
@@ -5,16 +5,19 @@ import com.github.payne.generator.annotations.NotTested;
 import com.github.payne.generator.input.GeneratorConfigs;
 import com.github.payne.generator.output.GeneratedProject;
 import com.github.payne.generator.output.vfs.VirtualFileSystem;
-import lombok.RequiredArgsConstructor;
 
 @NotTested
 @NotImplemented
-@RequiredArgsConstructor
 public class LogicProcessor {
 
     private GeneratorConfigs input;
     private final GeneratedProject project;
-    private final VirtualFileSystem vfs = project.getVirtualFileSystem();
+    private final VirtualFileSystem vfs;
+
+    public LogicProcessor(GeneratedProject project) {
+        this.project = project;
+        this.vfs = project.getVirtualFileSystem();
+    }
 
     public void applyInputs(GeneratorConfigs input) {
         /*
