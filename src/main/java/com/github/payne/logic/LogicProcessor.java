5ccbf2034ea1fb92be1b303542370897ef0ccef3
payne911 <payne911@users.noreply.github.com>
2020-09-16 02:03:04 -0400

    First piece of logic in LogicProcessor

--- src/main/java/com/github/payne/logic/LogicProcessor.java
+++ src/main/java/com/github/payne/logic/LogicProcessor.java
@@ -3,9 +3,12 @@ package com.github.payne.logic;
 import com.github.payne.generator.annotations.NotImplemented;
 import com.github.payne.generator.annotations.NotTested;
 import com.github.payne.generator.input.GeneratorConfigs;
+import com.github.payne.generator.input.model.enums.AddOn;
+import com.github.payne.generator.input.model.enums.Platform;
 import com.github.payne.generator.output.GeneratedProject;
 import com.github.payne.generator.output.vfs.FileNode;
 import com.github.payne.generator.output.vfs.SavableVfs;
+import java.util.Arrays;
 
 @NotTested
 @NotImplemented
@@ -16,6 +19,8 @@ public class LogicProcessor {
     private final SavableVfs vfs;
     private final FileNode root;
 
+    public static final String ASSETS_FOLDER = "assets";
+
     public LogicProcessor(GeneratorConfigs input, GeneratedProject project) {
         this.input = input;
         this.project = project;
@@ -25,32 +30,44 @@ public class LogicProcessor {
 
     public void applyInputs() {
         addBasicFiles();
-        addJvmLanguagesSupport();
-        addExtensions();
-        applyTemplate();
-        addPlatforms();
         addSkinAssets();
+        addLibraries();
+        applyTemplate();
+        input.getPlatforms().forEach(this::addPlatform);
+        addJvmLanguagesSupport();
         addReadmeFile();
         saveProperties();
         saveFiles();
     }
 
     public void addBasicFiles() {
+        vfs.copyFileToRoot(Arrays.asList("generator", "static", "gitignore"), ".gitignore");
+        root.addChild(new FileNode(ASSETS_FOLDER)); // there's always the main assets folder
     }
 
-    public void addJvmLanguagesSupport() {
+    public void addSkinAssets() {
+        if (input.getAddOns().contains(AddOn.GUI_ASSETS)) {
+            vfs.copyFolder(Arrays.asList("generator", "static", "assets"),
+                    Arrays.asList(ASSETS_FOLDER), false);
+        }
     }
 
-    public void addExtensions() {
+    public void addLibraries() {
     }
 
     public void applyTemplate() {
     }
 
-    public void addPlatforms() {
+    public void addPlatform(Platform platform) {
+        System.out.println("Applying " + platform);
+        System.out.println("Applying " + platform.ordinal());
+        System.out.println("Applying " + platform.name());
+        System.out.println("Applying " + platform.getValue());
+        System.out.println("Applying " + platform.toString());
+        System.out.println("=============================");
     }
 
-    public void addSkinAssets() {
+    public void addJvmLanguagesSupport() {
     }
 
     public void addReadmeFile() {
