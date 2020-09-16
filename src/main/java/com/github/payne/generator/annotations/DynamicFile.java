d14ee56c2269fcdb0241744118503a3ed295f69d
payne911 <payne911@users.noreply.github.com>
2020-09-16 18:13:47 -0400

    Precision on DynamicFile

--- src/main/java/com/github/payne/generator/annotations/DynamicFile.java
+++ src/main/java/com/github/payne/generator/annotations/DynamicFile.java
@@ -3,6 +3,9 @@ package com.github.payne.generator.annotations;
 /**
  * Decorator annotation to help navigate from a class to the resources file which it uses to
  * generate content dynamically.
+ * <p>
+ * Using your IDE, you should be able to {@code ctrl + click} on the file in the annotation and
+ * it'll automatically open it for you.
  */
 public @interface DynamicFile {
 
