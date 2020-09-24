db4297a8912104830c4ebac93aa0f67dd23e761a
payne911 <payne911@users.noreply.github.com>
2020-09-24 01:00:42 -0400

    html module 'sharedSourceSets'

--- src/main/java/com/github/payne/logic/modules/html/files/HtmlBuildGradleFile.java
+++ src/main/java/com/github/payne/logic/modules/html/files/HtmlBuildGradleFile.java
@@ -1,6 +1,7 @@
 package com.github.payne.logic.modules.html.files;
 
 import com.github.payne.generator.input.GeneratorConfigs;
+import com.github.payne.generator.input.model.enums.Platform;
 import com.github.payne.logic.root.BuildGradleFile;
 
 public class HtmlBuildGradleFile extends BuildGradleFile {
@@ -11,7 +12,18 @@ public class HtmlBuildGradleFile extends BuildGradleFile {
 
     @Override
     protected void assignOtherKeys() {
+        sharedSourceSets();
+
         // todo: add Third-Parties
 //        assignKey("dependencies", joinDependencies(dependencies, "api"));
+    }
+
+    private void sharedSourceSets() {
+        final String replacement = input.contains(Platform.SHARED)
+                ? "sourceSets.main.compileClasspath += files(project(':" + Platform.SHARED
+                .getValue()
+                + "').sourceSets.main.allJava.srcDirs)"
+                : "";
+        assignKey("sharedSourceSets", replacement);
     }
 }
